53559c12c8bdc685ede1d5bc4e96e94489dbc19d
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-09-18 12:21:29 +0200

    Added README.md, cleaned POM.xml and some small changes

---
+++ README.md
@@ -0,0 +1,15 @@
+# JavaFX Starter
+
+This project is meant to be a simple starter for JavaFX 17 under Maven. 
+It's not comprehensive, and it doesn't build to a runnable jar... yet.
+
+It is not assumed that you have Maven installed on your own machine,
+this project only runs in the Maven tab in IntelliJ (I'm assuming your are using that).
+
+Instructions:
+
+1. Clone this project
+2. Open up the Maven tab (on the right side of your screen)
+3. Perform: LifeCycle > clean + install
+4. Perform: Plugins > javafx > javafx:run
+5. Customize to your own needs
--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,10 +1,24 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setHeight(600);
+        stage.setWidth(800);
+        stage.setTitle("Inholland JavaFX Starter Project");
+
+        BorderPane pane = new BorderPane();
+        Label hello = new Label("Hello World");
+        pane.setCenter(hello);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
     }
 }
