fe07747eeaaba756a2997ee269fbefc7fde31fbb
paxer2k <70654308+paxer2k@users.noreply.github.com>
2021-09-23 00:34:08 +0200

    password criteria implementation

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,24 +1,106 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+    Label userLabel, passwordLabel;
+    TextField userInput;
+    PasswordField passwordField;
+    Button button;
+
     @Override
     public void start(Stage window) throws Exception {
-        window.setHeight(600);
-        window.setWidth(800);
-        window.setTitle("Inholland JavaFX Starter Project");
+        window.setHeight(200);
+        window.setWidth(320);
+        window.setTitle("Login screen");
 
-        BorderPane pane = new BorderPane();
-        Label hello = new Label("Hello World");
-        pane.setCenter(hello);
+        userLabel = new Label("Username");
+        passwordLabel = new Label("Password");
+        userInput = new TextField();
+        userInput.setPromptText("Username");
+        passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        button = new Button("Log in");
 
-        Scene scene = new Scene(pane);
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(8);
+        gridPane.setVgap(10);
+
+        // name // hor position // ver position
+        gridPane.add(userLabel, 0, 0);
+        gridPane.add(userInput, 1, 0);
+        gridPane.add(passwordLabel, 0, 1);
+        gridPane.add(passwordField, 1, 1);
+        gridPane.add(button, 1, 2);
+
+
+        Scene scene = new Scene(gridPane);
         window.setScene(scene);
         window.show();
+
+        button.setVisible(false);
+
+        StringProperty passwordProperty = passwordField.textProperty();
+        passwordProperty.addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue,
+                                String oldValue, String newValue) {
+
+                char[] chars = passwordField.getText().toCharArray();
+                boolean validLength = false;
+                boolean hasChar = false;
+                boolean hasDigit = false;
+                boolean hasSpecialChar = false;
+
+                String specialChars = "!@#$%&*()’+,-./:;<=>?[]^_`{|}";
+
+                for(char c : chars){
+
+                    // making 4 criterias
+                    if (passwordField.getText().length() >= 8){
+                        validLength = true;
+                    }
+                    if(Character.isDigit(c) == true){
+                        hasDigit = true;
+                    }
+                    if(Character.isLetter(c) == true){
+                        hasChar = true;
+                    }
+
+                    // converting the char into a string
+                    String s = String.valueOf(c);
+                    if (specialChars.contains(s)){
+                        hasSpecialChar = true;
+                    }
+
+                    // checking the four criterias
+                    if (((validLength == true) && (hasChar == true) && (hasDigit == true) && (hasSpecialChar == true))){
+                        button.setVisible(true);
+                    }
+                    else{
+                        button.setVisible(false);
+                    }
+
+                }
+            }
+        });
+
     }
+
+
+
 }
